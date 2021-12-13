ef4c6d46930b05d95373282f34d3b92dc5cfd5ae
KadeDede <dedenht76@gmail.com>
2021-12-13 12:00:13 +0800

    deploy 4

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from app import routes
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World! Deploy nich...</p>"
+
+@app.route("/works")
+def it_works():
+    return "IT Works! nyehehe"
+# from app import routes
 
 
-app.run(debug=True)
+# app.run(debug=True)
